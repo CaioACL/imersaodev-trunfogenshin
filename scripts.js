a953f485d28d553e7fe279d90f842ab13a900a0c
Caio Azevedo <87204701+CaioACL@users.noreply.github.com>
2021-07-09 18:50:25 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,319 @@
+var Per1 = {
+    nome: "Zhongli",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/12/portrait-zhongli.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/zhongli/1000400_03_jp.wav",
+    atributos: {
+        Vida: 36000,
+        Ataque: 1800,
+        Defesa: 800,
+        DanoElemental: 91,
+        
+    }
+}
+
+var Per2 = {
+    nome: "Diluc",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-diluc.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/diluc/1000400_02_jp.wav",
+    atributos: {
+        Vida: 18000,
+        Ataque: 3200,
+        Defesa: 730,
+        DanoElemental: 94,
+        
+    }
+}
+var Per3 = {
+    nome: "Hu Tao",
+    img: "https://genshinpro.com.br/wp-content/uploads/2021/03/portrait-hutao.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/hutao/1000400_03_jp.wav",
+    atributos: {
+        Vida: 31000,
+        Ataque: 2900,
+        Defesa: 700,
+        DanoElemental: 102,
+        
+    }
+}
+
+var Per4 = {
+    nome: "Ganyu",
+    img: "https://genshinpro.com.br/wp-content/uploads/2021/01/portrait-ganyu.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/ganyu/1000400_04_jp.wav",
+    atributos: {
+        Vida: 22000,
+        Ataque: 2450,
+        Defesa: 815,
+        DanoElemental: 90,
+        
+    }
+}
+
+var Per5 = {
+    nome: "Xiao",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-xiao.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/xiao/1000400_03_jp.wav",
+    atributos: {
+        Vida: 18000,
+        Ataque: 3100,
+        Defesa: 785,
+        DanoElemental: 91,
+        
+    }  
+}
+
+var Per6 = {
+    nome: "Mona",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-mona.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/mona/1000400_03_jp.wav",
+    atributos: {
+        Vida: 17500,
+        Ataque: 2700,
+        Defesa: 900,
+        DanoElemental: 91,
+        
+    }  
+}
+
+var Per7 = {
+    nome: "Tartaglia",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-tartaglia.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/tartaglia/1000401_03_jp.wav",
+    atributos: {
+        Vida: 18500,
+        Ataque: 2450,
+        Defesa: 780,
+        DanoElemental: 92,
+        
+    }  
+}
+
+var Per8 = {
+    nome: "Venti",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-venti.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/venti/1000400_01_jp.wav",
+    atributos: {
+        Vida: 17800,
+        Ataque: 1530,
+        Defesa: 850,
+        DanoElemental: 101,
+        
+    }  
+}
+
+var Per9 = {
+    nome: "Ningguang",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-ningguang.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/ningguang/1000400_03_jp.wav",
+    atributos: {
+        Vida: 16800,
+        Ataque: 2375,
+        Defesa: 1030,
+        DanoElemental: 89,
+    }
+}
+  
+var Per10 = {
+    nome: "Bennett",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-bennett.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/bennett/1000400_02_jp.wav",
+    atributos: {
+        Vida: 31000,
+        Ataque: 1800,
+        Defesa: 800,
+        DanoElemental: 60,
+    }
+}
+
+var Per11 = {
+    nome: "Albedo",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/12/portrait-albedo.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/albedo/1000400_03_jp.wav",
+    atributos: {
+        Vida: 22370,
+        Ataque: 1100,
+        Defesa: 2000,
+        DanoElemental: 90,
+    }
+}
+
+var Per12 = {
+    nome: "Razor",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-razor.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/razor/1000400_03_jp.wav",
+    atributos: {
+        Vida: 22400,
+        Ataque: 3050,
+        Defesa: 650,
+        DanoElemental: 0,
+    }
+}
+
+var Per13 = {
+    nome: "Klee",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-klee.jpg",
+    golpe: "https://genshin.honeyhunterworld.com/audio/quotes/klee/1000400_02_jp.wav",
+    atributos: {
+        Vida: 1645,
+        Ataque: 3010,
+        Defesa: 730,
+        DanoElemental: 95,
+    }
+}
+
+var Per14 = {
+    nome: "Lisa (SUPER TRUNFO)",
+    img: "https://genshinpro.com.br/wp-content/uploads/2020/11/portrait-lisa.jpg",
+    golpe: "https://www.myinstants.com/media/sounds/lisa_ara_ara_normalized_lGdXcjK.mp3",
+    atributos: {
+        Vida: 40000,
+        Ataque: 4000,
+        Defesa: 4000,
+        DanoElemental: 120,
+    }
+}
+
+var somEmpate = {
+  empate: "https://www.myinstants.com/media/sounds/ac_hidden_blade_1.mp3"
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [Per1, Per2, Per3, Per4, Per5, Per6, Per7, Per8, Per9, Per10, Per11, Per12, Per13, Per14]
+//            0           1           2          3         4            5            6           7     
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantCartas()
+
+
+
+
+function atualizaQuantCartas() {
+  var divQuantCartas = document.getElementById('quantidade-cartas') 
+  var html = "Quantidade de Cartas no Jogo: " + cartas.length
+  
+  divQuantCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + " / " + pontosMaquina + " Máquina"
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://i.imgur.com/kgDkxRu.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.img})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+        var golpe = new Audio(cartaJogador.golpe)
+        golpe.volume = 0.3
+        golpe.play()
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+        var golpe = new Audio(cartaMaquina.golpe)
+        golpe.volume = 0.3
+        golpe.play()
+        pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+        var empate = new Audio(somEmpate.empate)
+        empate.volume = 0.3
+        empate.play()
+    }
+  
+  if (cartas.length == 0){
+    alert("Fim de Jogo")
+    if (pontosJogador > pontosMaquina){
+      htmlResultado = '<p class="resultado-final">Parabéns você Venceu!</p>'
+    } else if (pontosMaquina) {
+      htmlResultado = '<p class="resultado-final">Você Foi Derrotado!</p>'
+    } else {
+      htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+  } else {
+    document.getElementById('btnProximaRodada').disabled = false
+  }
+    
+
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+  
+    atualizaPlacar()
+    exibeCartaMaquina()
+    atualizaQuantCartas()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://i.imgur.com/kgDkxRu.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.img})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        console.log(atributo)
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada() {
+  var divCartas = document.getElementById('cartas')
+  
+  divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>` 
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
